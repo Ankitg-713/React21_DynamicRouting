479e3e28b1ddd86b154be02bbaac23d4ef85afc5
ankitkumargupta <ankitgupta6881@gmail.com>
2025-01-12 19:37:31 +0530

    learned dynamic routing

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -15,7 +15,7 @@ function UserDetails() {
 
   return (
     <div className='w-full h-full flex flex-col items-center'>
-        <h1 className='text-6xl text-center text-blue-300 p-4'>User Details</h1>
+        <h1 className='text-6xl text-center text-blue-400 p-4'>User Details</h1>
         <h1 className='text-center text-3xl font-semibold'>Hii {name}</h1>
         <button onClick={goBackHandler} className='px-3 py-1 bg-red-400 rounded-md mt-4 text-sm text-white'>Go Back</button>
     </div>
